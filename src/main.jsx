43ae193c97af28b137a244a71526952bc8dd0c0f
zach-ss <zach.2045@gmail.com>
2026-06-25 13:30:46 +1000

    Remove index.css import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
 import Terms from './Terms.jsx'
 import Privacy from './Privacy.jsx'
