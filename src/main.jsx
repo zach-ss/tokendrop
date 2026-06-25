d666bc200dabc292b2b3ef6ed10e68cd57575132
zach-ss <zach.2045@gmail.com>
2026-06-25 13:25:37 +1000

    Add routing for Terms and Privacy pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
 import App from './App.jsx'
+import Terms from './Terms.jsx'
+import Privacy from './Privacy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
